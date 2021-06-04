259ed0acb6f8382f5db765809466975d1958e51f
Debosmita Ghosh <84208551+Debosmita-web@users.noreply.github.com>
2021-06-04 21:59:28 +0530

    Update password.py

--- password.py
+++ password.py
@@ -1,15 +1,20 @@
-from tkinter import *
+from tkinter import * 
 import random, string
 import pyperclip
-root=Tk()
-root.geometry("400x400")
-root.resizable(0,0)
-root.title("DataFlair - PASSWORD GENERATOR")
+
+root=Tk()#Tk() initialized tkinter which means window created
+root.geometry("400x400")#geometry() set the width and height of the window
+root.resizable(0,0)#resizable(0,0) set the fixed size of the window
+root.title("DataFlair - PASSWORD GENERATOR")#title() set the title of the window
+
+#Label() widget use to display one or more than one line of text that users can’t able to modify.
 Label(root, text = 'PASSWORD GENERATOR' , font ='arial 15 bold').pack()
 Label(root, text ='DataFlair', font ='arial 15 bold').pack(side = BOTTOM)
-pass_label = Label(root, text = 'PASSWORD LENGTH', font = 'arial 10 bold').pack()
-pass_len = IntVar()
-length = Spinbox(root, from_ = 8, to_ = 32 , textvariable = pass_len , width = 15).pack()
+pass_label = Label(root, text = 'PASSWORD LENGTH', font = 'arial 10 bold').pack() 
+pass_len = IntVar() #pass_len is an integer type variable that stores the length of a pass 
+length = Spinbox(root, from_ = 8, to_ = 32 , textvariable = pass_len , width = 15).pack() #Spinbox() widget is used to select from a fixed number of values. Here the value from 8 to 32words.
+
+#Function to generate password
 pass_str = StringVar()
 def Generator():
 	password = ''
@@ -20,8 +25,10 @@ def Generator():
 	pass_str.set(password)
 
 	
-Button(root, text = "GENERATE PASSWORD" , command = Generator ).pack(pady= 5)
-Entry(root , textvariable = pass_str).pack()
+Button(root, text = "GENERATE PASSWORD" , command = Generator ).pack(pady= 5) #Button() widget used to display button on our window
+Entry(root , textvariable = pass_str).pack() #Entry() widget used to create an input text field
+
+#Function to Copy Password
 def Copy_password():
         pyperclip.copy(pass_str.get())
 
